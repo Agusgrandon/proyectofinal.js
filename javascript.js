70929483c53aeb26b459d41bf5fb3d0b8e4c4acc
agustina grandon <agustina107.grandon@gmail.com>
2022-04-25 19:23:36 -0300

    agrege js

--- javascript.js
+++ javascript.js
@@ -1,23 +1,49 @@
 /* carrito */ 
+class Producto {
+    constructor(nombre, celular, precio, cantidad) {
+        this.celular= celular
+        this.precio = precio
+        this.cantidad = cantidad
+        this.nombre = nombre
+    }
+}
+/* carrito */  
+let li
+let divCarrito = document.getElementById("carrito")
+let divDelLi =document.getElementById("lista")
 
+function crearDropdown(divi){
+    for (i of JSON.parse(localStorage.getItem("carrito"))){
+        li = document.createElement("li")
+        li.innerHTML = `
+          <div class="filaDelCarrito d-flex justify-content-evenly">
+            <p class="parrafo" >${i.celular}<p>
+            <p class="parrafo" >${i.cantidad} und.<p>
+            <button class="btn" id="btn-eliminar${i.id}"></button>
+            <p class="parrafo">$${i.precio*i.cantidad}<p>
+          <div>
+          `
+        divi.append(li)
+    }
+}
+/* carrito */ 
+function actualizarDom(div, cantidades) {
+    let cantidad = div.querySelector(".cantidad")
+    cantidad.innerHTML = cantidades
+}
 
-
-
-
-
-
-
-/* evento de la card */ 
-document.getElementById("agregarProducto").addEventListener("click", function(){
-                       const celular = document.getElementById("celular");
-                       const precio = document.getElementById("precio");
-
-                       console.log(new Product(celular, precio));
-
-})
-
-
-
+function irAlCarrito(producto){
+    let comprar = document.getElementById("boton")
+    comprar.onclick = () => {
+        if (carrito.find((el) => el.nombre == producto.celular)){
+            producto.cantidad++;
+        }
+        else{
+            carrito.push(producto)
+        }
+    
+    }
+}
 
 
 
