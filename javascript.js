9cb8aeccf7d6715feb083ef0e2b069356750c5fb
agustina grandon <agustina107.grandon@gmail.com>
2022-04-25 13:33:30 -0300

    agrege js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 /* evento del formulario */ 
 document.getElementById("formulario").addEventListener("submit", function (e) {
     const mail = document.getElementById("mail").value;
-    alert("Suscripto!");
+    if(mail === ""){
+        alert("completa el dato");
+    } else {
+        alert("suscripto!");
+    }
+ 
  
     e.preventDefault();
  })
+ /* termino el evento */ 
