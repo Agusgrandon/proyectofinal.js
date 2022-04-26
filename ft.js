3caa04a161c963b9b4dc970fe122f9a731eafcb2
agustina grandon <agustina107.grandon@gmail.com>
2022-04-26 16:49:00 -0300

    modifique js

---
+++ ft.js
@@ -0,0 +1,12 @@
+let productos = [{ id: 1,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 2,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 3,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 4,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 5,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 6,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 7,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 8,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 9,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 10, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 11, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 12, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"}];
--- javascript.js
+++ javascript.js
@@ -1,16 +1,3 @@
-let productos = [{ id: 1,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 2,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 3,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 4,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 5,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 6,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 7,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
-                 { id: 8,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
-                 { id: 9,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
-                 { id: 10, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
-                 { id: 11, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
-                 { id: 12, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"}];
-
 
 /* para insertar las cards */
 const contenedorProductos = document.getElementById("contenedor")
@@ -31,20 +18,18 @@ productos.forEach((producto) => {
     
     `
     contenedorProductos.appendChild(div)
-    
+
+   
 });
 
-/* hago una funcion para agregar la card al carrito*/
 
+
+/* hago una funcion para agregar la card al carrito*/
 const agregarAlCarrito = (prodId) => {
     const item = productos.find ((prod) => prod.id === prodId)
     carrito.push(item)
     console.log("carrito");
 }
-
-
-
-
 
 /* evento del formulario */ 
 document.getElementById("formulario").addEventListener("submit", function (e) {
