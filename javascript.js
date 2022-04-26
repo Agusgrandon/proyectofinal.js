c2477099b0fb30b88f4edf1ad7a967106f680779
agustina grandon <agustina107.grandon@gmail.com>
2022-04-26 12:30:07 -0300

    agrege una foto

--- javascript.js
+++ javascript.js
@@ -3,7 +3,15 @@ let productos = [{ id: 1,  nombre: "Celular Samsung A52", precio: 72000, img: ".
                  { id: 3,  nombre: "Celular Samsung A52", precio: 72000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
                  { id: 4,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
                  { id: 5,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
-                 { id: 6,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"}];
+                 { id: 6,  nombre: "Celular Samsung A73", precio: 80000, img: "./img/celulares/CGG_Samsung-Galaxy-A52.jpg"},
+                 { id: 7,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 8,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 9,  nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 10, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 11, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"},
+                 { id: 12, nombre: "Celular Xiaomi Mi 11", precio: 90000, img: "./img/celulares/xiaomi-11t.jpg"}];
+
+
 /* para insertar las cards */
 const contenedorProductos = document.getElementById("contenedor")
 /* let carrito*/
