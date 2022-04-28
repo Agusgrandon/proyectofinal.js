b55ba8721733f2fd68b332c687903c48a8082430
agustina grandon <agustina107.grandon@gmail.com>
2022-04-27 22:05:52 -0300

    agrege contenido

--- java1.js
+++ java1.js
@@ -1,1 +1,49 @@
+class Carrito {
 
+    /*  para añadir un producto al carrito */ 
+    comprarProducto(e){
+        e.preventDefault();
+        /* apretando el boton se agrega el producto */ 
+        if(e.target.classList.contains('agregar-carrito')){
+            /*se almacena el producto que seleccione en esta const */ 
+            const producto = e.target.parentElement.parentElement;
+             /*para leer el producto */ 
+             this.leerDatosProducto(producto);
+            
+        }
+    }
+    /* indico los datos del producto para que los agrege */ 
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector("img").src,
+            titulo: producto.querySelector("h5").textContent,
+            precio: producto.querySelector(".precio").textContent,
+            id: producto.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+
+        this.insertarCarrito(infoProducto);
+    }
+     /* para que aparesca en el carrito */ 
+     insertarCarrito(producto){
+         const row = document.createElement("tr");
+         row.innerHTML =  ` 
+         <td><img src="${producto.imagen}" alt="#" width=50></td>
+         <td>${producto.titulo}</td>
+         <td>${producto.precio}</td>
+         <td><button class="borrar-producto btn btn-danger" data-id="${producto.id}">Eliminar</button></td>
+         `;
+        listaProductos.appendChild(row);
+     }
+    /* para que eliminar producto del carrito */ 
+    eliminarProducto(e){
+        e.preventDefault();
+        let producto, productoID;
+        if(e.target.classList.contains("borrar-producto")){
+            e.target.parentElement.parentElement.remove();
+            producto = e.target.parentElement.parentElement;
+        }
+        
+    }
+}
+   
--- java2.js
+++ java2.js
@@ -1,4 +1,18 @@
+const carro = new Carrito();
+const carrito = document.getElementById('carrito');
+const productos = document.getElementById('lista-productos');
+const listaProductos = document.querySelector('#lista-carrito tbody');
 
+cargarEventos();
+
+function cargarEventos(){
+
+    /* este se ejecuta cuando se agrega al carrito */
+    productos.addEventListener('click', (e)=>{carro.comprarProducto(e)});
+
+     /* este se ejecuta cuando se elimina del carrito*/
+    carrito.addEventListener('click', (e)=>{carro.eliminarProducto(e)});
+}
 
 /* evento del formulario */ 
 document.getElementById("formulario").addEventListener("submit", function (e) {
